1a36fbe101c6411a58101e51a4d7090620c22359
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-24 12:36:28 +0100

    version finale pour le war

--- src/main/java/com/sip/ams/AmsApiApplication.java
+++ src/main/java/com/sip/ams/AmsApiApplication.java
@@ -2,9 +2,10 @@ package com.sip.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmsApiApplication {
+public class AmsApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmsApiApplication.class, args);
